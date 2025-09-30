8ad231978624185851df9d89ae7f45a408b24fe2
mrtaichi <isaaniet@gmail.com>
2025-09-29 23:16:54 -0600

    fix: More time to the timeout

--- linPEAS/buildCustom.sh
+++ linPEAS/buildCustom.sh
@@ -7,7 +7,7 @@ python3 -m builder.linpeas_builder \
   --output /tmp/linpeas_tmp.sh
 
 echo '#!/bin/bash' > /tmp/linpeas_ctf.sh
-echo 'TIMEOUT=60' >> /tmp/linpeas_ctf.sh
+echo 'TIMEOUT=240' >> /tmp/linpeas_ctf.sh
 echo '( sleep $TIMEOUT && echo "Timeout alcanzado" && kill $$ ) &' >> /tmp/linpeas_ctf.sh
 echo 'KILLER_PID=$!' >> /tmp/linpeas_ctf.sh
 cat /tmp/linpeas_tmp.sh >> /tmp/linpeas_ctf.sh  # Agrega el contenido
